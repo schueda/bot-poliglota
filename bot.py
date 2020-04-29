3eba84baca30ff9d9d37aab44683e583907bce9e
Andre Schueda <a.schuedamenezes@gmail.com>
2020-04-29 18:54:13 -0300

    cria a estrutura basica do bot, obtem as mentions e processa emojis

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import tweepy
 import os
+import time
+import emoji
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -14,6 +16,27 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+last_id = None
+
+while True:
+
+    if last_id != None:
+        print(f"\n----- Puxando mentions desde {last_id} -----")
+        mentions = api.mentions_timeline(since_id=last_id)
+    else:
+        print("\n\n----- Puxando todas as mentions -----")
+        mentions = api.mentions_timeline()
+   
+    if len(mentions) != 0:
+        last_id = mentions[0].id
+
+    print("\n----- IMPRIMINDO MENTIONS -----")
+    for status in mentions:
+        print(status.id, status.text)
+        #decode  = status.text.decode('utf-8')
+        decode  = status.text
+        allchars = [str for str in decode]
+        emojis = [c for c in allchars if c in emoji.UNICODE_EMOJI]
+        print(emojis)
+    time.sleep(10)
+
