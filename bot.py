75302d88a166ad91a9dec21aea8f4ca5a217b122
Andre Schueda <a.schuedamenezes@gmail.com>
2020-04-30 19:26:46 -0300

    Protótipo da função que relaciona qual bandeira é relacionada a qual lingua. Separador de emojis de bandeiras dos emojis diversos

--- bot.py
+++ bot.py
@@ -6,6 +6,18 @@ import emoji
 from dotenv import load_dotenv
 load_dotenv()
 
+
+def language_flag(flag_code):
+    if flag_code == "a":
+        return "english"
+    elif flag_code == "b":
+        return "german"
+    elif flag_code == "c":
+        return "italian"
+    else:
+        return 0
+
+
 consumer_key = os.getenv("key")
 consumer_secret = os.getenv("secret")
 access_token = os.getenv("token")
@@ -16,8 +28,8 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-last_id = None
 
+last_id = None
 while True:
 
     if last_id != None:
@@ -32,11 +44,19 @@ while True:
 
     print("\n----- IMPRIMINDO MENTIONS -----")
     for status in mentions:
-        print(status.id, status.text)
+        #print(status.id, status.text)
         #decode  = status.text.decode('utf-8')
         decode  = status.text
         allchars = [str for str in decode]
         emojis = [c for c in allchars if c in emoji.UNICODE_EMOJI]
-        print(emojis)
+        converted_to_ascii = [ord(c) for c in emojis]
+        print("símbolo", emojis)
+        print("na ascii", converted_to_ascii)
+        bandeiras=[]
+        for cod in converted_to_ascii:
+            if cod >= 127462 and cod <= 127487:
+                bandeiras.append(str(cod))
+        print("caracteres de bandeira", bandeiras, "\n")
+    
     time.sleep(10)
 
