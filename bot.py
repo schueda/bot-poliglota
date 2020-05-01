3f2dedffc635513d2bc5fa0b5526fbab77a59880
Enzo Maruffa <enzomm1999@gmail.com>
2020-04-30 22:42:51 -0300

    Adds lots of comments

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ with open('languages.json') as json_file:
     print("estados unidos:", languages["127482127480"]) 
     print("suiça:", languages["127464127469"]) 
 
-
+# Renomeie pra um verbo tipo "filterFlags"
 def emojis_flags_separation(ascii_codes):
     flags = []
     for cod in ascii_codes:
@@ -19,6 +19,7 @@ def emojis_flags_separation(ascii_codes):
                 flags.append(str(cod))
     return flags
 
+# Renomeie pra um verbo tipo "uniteFlags" ou algo nessa linha
 def flags_junction(separated_flags):
     united_flags = []
     i = 0
@@ -62,49 +63,69 @@ def flags_junction(separated_flags):
 #     return language_list()
 
 
-# load_dotenv()
+load_dotenv()
 
-# consumer_key = os.getenv("key")
-# consumer_secret = os.getenv("secret")
-# access_token = os.getenv("token")
-# access_token_secret = os.getenv("token_secret")
+consumer_key = os.getenv("key")
+consumer_secret = os.getenv("secret")
+access_token = os.getenv("token")
+access_token_secret = os.getenv("token_secret")
 
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-# api = tweepy.API(auth)
+api = tweepy.API(auth)
 
+last_id = None
 
-# last_id = None
-# while True:
+while True:
 
-#     if last_id != None:
-#         print(f"\n----- Puxando mentions desde {last_id} -----")
-#         mentions = api.mentions_timeline(since_id=last_id)
-#     else:
-#         print("\n\n----- Puxando todas as mentions -----")
-#         mentions = api.mentions_timeline()
+    ## Transforme em uma função que retorna as mentions
+    if last_id != None:
+        print(f"\n----- Puxando mentions desde {last_id} -----")
+        mentions = api.mentions_timeline(since_id=last_id)
+    else:
+        print("\n\n----- Puxando todas as mentions -----")
+        mentions = api.mentions_timeline()
    
-#     if len(mentions) != 0:
-#         last_id = mentions[0].id
+    if len(mentions) != 0:
+        last_id = mentions[0].id
 
-#     print("\n----- IMPRIMINDO MENTIONS -----")
-#     for status in mentions:
-#         #print(status.id, status.text)
-        
-#         decode  = status.text #decode  = status.text.decode('utf-8')
-#         allchars = [str for str in decode]
-#         emojis = [c for c in allchars if c in emoji.UNICODE_EMOJI]
-#         print("símbolo         ", emojis)
-        
-#         converted_to_ascii = [ord(c) for c in emojis]
-#         print("na ascii        ", converted_to_ascii)
-        
-#         pure_divided_flags = emojis_flags_separation(converted_to_ascii)
-#         print("apenas bandeiras", pure_divided_flags)
+    print("\n----- IMPRIMINDO MENTIONS -----")
+    for status in mentions:
+        #print(status.id, status.text)
 
-#         flags = flags_junction(pure_divided_flags)
-#         print("bandeiras juntas", flags, "\n")
+        # Adicione uma verificação pra ver se a mention começa com o @ do bot
+        
+        ## Transforme em una função que faça esse processo em outro lugar
+        # Daqui
+        decode  = status.text #decode  = status.text.decode('utf-8')
+        allchars = [str for str in decode]
+        emojis = [c for c in allchars if c in emoji.UNICODE_EMOJI]
+        print("símbolo         ", emojis)
+        
+        converted_to_ascii = [ord(c) for c in emojis]
+        print("na ascii        ", converted_to_ascii)
+        
+        pure_divided_flags = emojis_flags_separation(converted_to_ascii)
+        print("apenas bandeiras", pure_divided_flags)
 
-#     time.sleep(10)
+        flags = flags_junction(pure_divided_flags)
+        # Até aqui
+
+        print("bandeiras juntas", flags, "\n")
+
+        # Loop de cada bandeira
+            # Pega idioma daquele país
+            # Loop de cada idioma
+                # Verifica se ja existe no buffer
+                    # Caso não, traduz pro idioma
+
+                # Guarda em um buffer (caso a pessoa coloque duas bandeiras de países que falam o mesmo idioma)
+
+                # Tweeta (cuidado com tweets muito longos)
+                
+        # Limpa buffer de idiomas
+
+
+    time.sleep(10)
 
